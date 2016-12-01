54c7a1d002f7fe2783f6ed1a513c70b99dff972c
Jo Kerkhofs <jke@thinkerit.be>
2016-12-01 14:05:23 +0100

    Moved templates js file to js folder

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ gulp.task('bootstrap3', function () {
     gulp.src('template/bootstrap3/*.html')
         .pipe(minifyHtml(minifyHtmlOpts))
         .pipe(templateCache('images-tpls-bootstrap3.js', {standalone: true, module: 'image-management.templates'}))
-        .pipe(gulp.dest('src'));
+        .pipe(gulp.dest('src/main/js'));
 });
 
 gulp.task('default', ['bootstrap3']);
